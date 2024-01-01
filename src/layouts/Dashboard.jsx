9ec7725eb1a252aff4b37c1d0a1eef4f41ab4f5b
Inky-Whale <berk.karanfiloglu@gmail.com>
2024-01-01 18:59:30 +0300

    components added

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Icon, Label } from "semantic-ui-react";
+import Navi from "./Navi";
+import Categories from "./Categories";
+import ProductList from "../pages/ProductList";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Categories />
+      <ProductList />
+    </div>
+  );
+}
---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { MenuMenu, MenuItem, Button, Menu, Container } from "semantic-ui-react";
+import CartSummary from "./CartSummary";
+
+export default function Navi() {
+  return (
+    <div>
+      <Menu inverted fixed size="">
+        <Container>
+          <MenuItem name="home" />
+          <MenuItem name="messages" />
+          <MenuItem name="messages" />
+
+          <MenuMenu position="right">
+            <CartSummary />
+            <MenuItem>
+              <Button primary>Sign Up</Button>
+            </MenuItem>
+          </MenuMenu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
